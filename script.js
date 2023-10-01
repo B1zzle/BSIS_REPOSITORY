d09bf2f17660dbf5e73c5d7a3c68c84edeab961c
Bastian Bragi Mailed Flores <108668352+B1zzle@users.noreply.github.com>
2023-10-01 19:59:29 +0800

    still fixing the profile view and about me page, added media queries for hamburger

--- script.js
+++ script.js
@@ -42,9 +42,8 @@ logoHome.addEventListener('mouseleave', () => {
 
 /* Button Animation Fix */
 window.addEventListener('DOMContentLoaded', () => {
-    let button = document.querySelector('button'); // Replace 'button' with your actual button selector
-
-    // Your existing animation code
+    let button = document.querySelector('button'); 
+   
     setTimeout(() => {
         logoSpan.forEach((span, idx) => {
             setTimeout(() => {
@@ -63,7 +62,35 @@ window.addEventListener('DOMContentLoaded', () => {
 
         setTimeout(() => {
             intro.style.top = '-100vh';
-            button.style.visibility = 'visible'; // Set button visibility to visible after animation completes
+            button.style.visibility = 'visible'; 
         }, 2500);
     });
+});
+
+/* Hamburger Menu */
+
+const Nav = document.querySelector('.nav-bar');
+const openMenu = document.querySelector('.hamburger');
+const closeMenu = document.querySelector('.exitMenu');
+
+openMenu.addEventListener('click', open);
+closeMenu.addEventListener('click', close);
+
+function open(event) {
+    event.stopPropagation(); // Prevent clicks from reaching the body and closing the menu
+    Nav.style.display = 'flex';
+    Nav.style.right = '0';
+    Nav.style.zIndex = '2'; // Bring the menu to the front
+}
+
+function close() {
+    Nav.style.display = 'none';
+    Nav.style.zIndex = '-1'; // Send the menu to the back
+}
+
+// Close the menu when clicking outside of it
+document.body.addEventListener('click', function(event) {
+    if (!Nav.contains(event.target)) {
+        close();
+    }
 });
